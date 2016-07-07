36f38d0f45bc6f6c8728ebf9b3ea76083c9f66bd
redst4r <redst4r@web.de>
2016-07-08 00:58:14 +0200

    useful pickling functions added

--- NetworkTrainer.py
+++ NetworkTrainer.py
@@ -221,3 +221,28 @@ class NetworkTrainer(object):
         plt.plot(self.valAccuracy, 'g-')
         plt.xlabel("epoch")
         plt.ylabel("Accuracy")
+
+
+
+# params.py: Implements IO functions for pickling network parameters.
+#
+import pickle
+import os
+import lasagne as nn
+PARAM_EXTENSION = 'params'
+
+def read_model_data(model, filename):
+    """Unpickles and loads parameters into a Lasagne model."""
+    filename = os.path.join('./', '%s.%s' % (filename, PARAM_EXTENSION))
+    with open(filename, 'r') as f:
+        data = pickle.load(f)
+    nn.layers.set_all_param_values(model, data)
+
+
+def write_model_data(model, filename):
+    """Pickels the parameters within a Lasagne model."""
+    data = nn.layers.get_all_param_values(model)
+    filename = os.path.join('./', filename)
+    filename = '%s.%s' % (filename, PARAM_EXTENSION)
+    with open(filename, 'w') as f:
+        pickle.dump(data, f)
